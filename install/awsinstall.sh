2addda14e068c562b87348707ea35ba34627ae51
cloudy <avcloudy@gmail.com>
2025-07-29 18:05:35 +1000

    automatic shutdown

--- install/awsinstall.sh
+++ install/awsinstall.sh
@@ -1,11 +1,11 @@
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
-bash Miniforge3-$(uname)-$(uname -m).sh -p "${HOME}/conda"
+bash Miniforge3-$(uname)-$(uname -m).sh -b -p "${HOME}/conda"
 rm Miniforge3-$(uname)-$(uname -m).sh
 source "${HOME}/conda/etc/profile.d/conda.sh"
 source "${HOME}/conda/etc/profile.d/mamba.sh"
 conda init
 mamba shell init
-conda env create -f ngpipe.yaml
+conda env create -f ngpipe/install/ngpipe.yaml
 git clone https://github.com/qhgenomics/pyngoST.git
 git clone https://github.com/kjolley/BIGSdb_downloader.git
 mkdir -p ${HOME}/.local/bin
