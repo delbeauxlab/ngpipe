4895e3479ef3c86551e922504dc3761530f850bf
cloudy <avcloudy@gmail.com>
2025-08-05 18:01:54 +1000

    absolute paths for multiple runs

--- scripts/multiple_run.sh
+++ scripts/multiple_run.sh
@@ -9,5 +9,5 @@ do
     tar -xzvf $tarball
     ls 2>/dev/null -Ub1 -- $id/*.fasta > $id/sample.txt
     tmux new-session -d -s "$id"
-    tmux send-keys -t "$id" "snakemake --snakefile ngpipe/Snakefile --config workdir=ngp$id contig_dir=$id samples=$id/samples.txt shutdown=multiple --cores $cores" Enter
+    tmux send-keys -t "$id" "snakemake --snakefile ngpipe/Snakefile --config workdir=/home/ubuntu/ngp$id contig_dir=/home/ubuntu/$id samples=/home/ubuntu/$id/samples.txt mlst_db=/home/ubuntu/data/ngono/mlst_database shutdown=multiple --cores $cores" Enter
 done
