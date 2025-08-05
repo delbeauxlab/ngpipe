101ab153392f04a95acb4909c5cb1ad6dc4d8295
cloudy <avcloudy@gmail.com>
2025-08-05 18:02:50 +1000

    tmux activate environment first

--- scripts/multiple_run.sh
+++ scripts/multiple_run.sh
@@ -9,5 +9,6 @@ do
     tar -xzvf $tarball
     ls 2>/dev/null -Ub1 -- $id/*.fasta > $id/sample.txt
     tmux new-session -d -s "$id"
+    tmux send-keys -t "$id" "conda activate ngpipe" Enter
     tmux send-keys -t "$id" "snakemake --snakefile ngpipe/Snakefile --config workdir=/home/ubuntu/ngp$id contig_dir=/home/ubuntu/$id samples=/home/ubuntu/$id/samples.txt mlst_db=/home/ubuntu/data/ngono/mlst_database shutdown=multiple --cores $cores" Enter
 done
