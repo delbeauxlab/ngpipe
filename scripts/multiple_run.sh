44bdb720e202242a768733114ecbeffe155d3860
cloudy <avcloudy@gmail.com>
2025-08-05 18:40:03 +1000

    need to strip .fasta from sample.txt entries

--- scripts/multiple_run.sh
+++ scripts/multiple_run.sh
@@ -8,7 +8,9 @@ do
     id=$(basename $tarball .tar.gz)
     mkdir $id
     tar -xzvf $tarball -C $id
-    ls 2>/dev/null -Ub1 -- $id/*.fasta > $id/sample.txt
+    cd $id
+    ls 2>/dev/null -Ub1 -- *.fasta | sed 's/.fasta//g' > sample.txt
+    cd ..
     tmux new-session -d -s "$id"
     tmux send-keys -t "$id" "conda activate ngpipe" Enter
     tmux send-keys -t "$id" "snakemake --snakefile ngpipe/Snakefile --config workdir=/home/ubuntu/ngp$id contig_dir=/home/ubuntu/$id samples=/home/ubuntu/$id/samples.txt mlst_db=/home/ubuntu/data/ngono/mlst_database shutdown=multiple --cores $cores" Enter
