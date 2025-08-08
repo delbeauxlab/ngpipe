04fb49be95d25f00a28229ca3136767bb309e519
cloudy <avcloudy@gmail.com>
2025-08-08 20:03:40 +1000

    update db before running in multiple runs

--- scripts/multiple_run.sh
+++ scripts/multiple_run.sh
@@ -3,6 +3,8 @@
 tarcount=$(ls 2>/dev/null -Ub1 -- ~/*.tar.gz | wc -l)
 cores=$(( 16/$tarcount ))
 
+counter=0
+
 for tarball in ~/*.tar.gz
 do
     id=$(basename $tarball .tar.gz)
@@ -11,7 +13,19 @@ do
     cd $id
     ls 2>/dev/null -Ub1 -- *.fasta | sed 's/.fasta//g' > samples.txt
     cd ..
+    if [ $counter -eq 0 ]
+    then
+        snakemake --snakefile ngpipe/Snakefile \
+            --config workdir=/home/ubuntu/ngp$id \
+            contig_dir=/home/ubuntu/$id \
+            samples=/home/ubuntu/$id/samples.txt \
+            mlst_db=/home/ubuntu/data/ngono/mlst_database \
+            update_db=true shutdown=multiple \
+            --cores $cores \
+            --until update_db_all
+        $counter=1
+    fi
     tmux new-session -d -s "$id"
     tmux send-keys -t "$id" "conda activate ngpipe" Enter
-    tmux send-keys -t "$id" "snakemake --snakefile ngpipe/Snakefile --config workdir=/home/ubuntu/ngp$id contig_dir=/home/ubuntu/$id samples=/home/ubuntu/$id/samples.txt mlst_db=/home/ubuntu/data/ngono/mlst_database shutdown=multiple --cores $cores" Enter
+    tmux send-keys -t "$id" "snakemake --snakefile ngpipe/Snakefile --config workdir=/home/ubuntu/ngp$id contig_dir=/home/ubuntu/$id samples=/home/ubuntu/$id/samples.txt mlst_db=/home/ubuntu/data/ngono/mlst_database update_db=false shutdown=multiple --cores $cores" Enter
 done
